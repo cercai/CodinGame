cef82607d042f03f717438ebf5946cd7040562c4
aheim <antoineheim@orange.fr>
2024-07-07 16:49:51 +0200

    Mars lander 2: test1 and 3 success

--- medium/mars-lander-2.py
+++ medium/mars-lander-2.py
@@ -2,9 +2,9 @@ import sys
 import math
 
 
-def debug(x, y, h_speed, v_speed, fuel, rotate, power):
+def debug(x, y, Vx, Vy, fuel, rotate, power):
     print("coordinates: [" + str(x) + ", " + str(y) + "]", file=sys.stderr, flush=True)
-    print("speed: [" + str(h_speed) + ", " + str(v_speed) + "]", file=sys.stderr, flush=True)
+    print("speed: [" + str(Vx) + ", " + str(Vy) + "]", file=sys.stderr, flush=True)
     print("fuel: " + str(fuel), file=sys.stderr, flush=True)
     print("rotate: " + str(rotate), file=sys.stderr, flush=True)
     print("power: " + str(power), file=sys.stderr, flush=True)
@@ -51,23 +51,82 @@ def get_max_speed(x):
         return MAX_SPEED[0]
     elif x >= DX[-1]:
         return MAX_SPEED[-1]
-    
+
+def radio_altitude(x, y, land_x, land_y):
+    for i, j in enumerate(land_x):
+        if i >= 1:
+            if  land_x[i-1] <= x and x <= land_x[i]:
+                a = (land_y[i] - land_y[i - 1]) / (land_x[i] - land_x[i-1])
+                b = land_y[i-1]
+                alt =  y - (a * (x-land_x[i-1]) + b)
+
+                return alt
+
+def ground_proctection(x, y, Vx, Vy, land_x, land_y):
+    # if dh and dh < 500:
+    future_x = x + 5*Vx
+    future_y = y + 5*Vy
+
+    future_x = max(0, future_x)
+    future_y = max(0, future_y)
+
+    print("x" + str(x), file=sys.stderr, flush=True)
+    print("future_x" + str(future_x), file=sys.stderr, flush=True)
+
+    print("y" + str(y), file=sys.stderr, flush=True)
+    print("future_y" + str(future_y), file=sys.stderr, flush=True)
+
+    if radio_altitude(future_x, future_y, land_x, land_y) < 700:
+        for i, j in enumerate(land_x):
+            if i >= 1:
+                if  land_x[i-1] <= future_x and future_x <= land_x[i]:
+                    a = (land_y[i] - land_y[i - 1]) / (land_x[i] - land_x[i-1])
+                    return 90 * math.sin(a)
+                    print("ground_proctection", file=sys.stderr, flush=True)
+                    print("a " + str(a), file=sys.stderr, flush=True)
+                    print("90 * math.sin(a) " + str(90 * math.sin(a)), file=sys.stderr, flush=True)
+                    # 90 * math.sin(a)
+                    # b = land_y[i-1]
+                    # alt =  y - (a * (future_x-land_x[i-1]) + b)
+
+                    # return alt
+            
+        
+
+
+    # print("land_y[i]" + str(land_y[i]), file=sys.stderr, flush=True)
+    # print("land_y[i-1]" + str(land_y[i-1]), file=sys.stderr, flush=True)
+    # print("land_y[i]-land_y[i-1]" + str(land_y[i]-land_y[i-1]), file=sys.stderr, flush=True)
+    # rotation = 90 * math.sin(Vx/speed)
+    # power = 4
+
+    # for i, j in enumerate(land_x):
+    #     if i >= 1:
+    #         if  land_x[i-1] <= x and x <= land_x[i]:
+    #             a = (land_y[i] - land_y[i - 1]) / (land_x[i] - land_x[i-1])
+    #             b = land_y[i-1]
+    #             alt =  y - (a * (x-land_x[i-1]) + b)
+
+    #             return alt
 
 # game loop
 while True:
-    # h_speed: the horizontal speed (in m/s), can be negative.
-    # v_speed: the vertical speed (in m/s), can be negative.
+    # Vx: the horizontal speed (in m/s), can be negative.
+    # Vy: the vertical speed (in m/s), can be negative.
     # fuel: the quantity of remaining fuel in liters.
     # rotate: the rotation angle in degrees (-90 to 90).
     # power: the thrust power (0 to 4).
-    x, y, h_speed, v_speed, fuel, rotate, power = [int(i) for i in input().split()]
-    debug(x, y, h_speed, v_speed, fuel, rotate, power)
+    x, y, Vx, Vy, fuel, rotate, power = [int(i) for i in input().split()]
+
+    dh = radio_altitude(x, y, land_x, land_y)
+    debug(x, y, Vx, Vy, fuel, rotate, power)
 
     dx = x - landing_site
-    dh = y - landing_height
+    dy = y - landing_height
 
+    # print("alt: " + str(alt), file=sys.stderr, flush=True)
     print("dx: " + str(dx), file=sys.stderr, flush=True)
-    print("dh: " + str(dh), file=sys.stderr, flush=True)
+    print("dy: " + str(dy), file=sys.stderr, flush=True)
 
     rotation = 0
     power = 4
@@ -85,39 +144,48 @@ while True:
         rotation = DX_ROT[-2]
 
 
-    DH_LIM = [2000, 1500, 1000, 500]
-    DH_POW = [3, 3, 4, 4]
+    DY_LIM = [2000, 1500, 1000, 500]
+    DY_POW = [2, 3, 4, 4]
 
-    if dh > DH_LIM[0]:
-        power = DH_POW[0]
-    elif dh > DH_LIM[1]:
-        power = DH_POW[1]
-    elif dh > DH_LIM[2]:
-        power = DH_POW[2]
+    if dy > DY_LIM[0]:
+        power = DY_POW[0]
+    elif dy > DY_LIM[1]:
+        power = DY_POW[1]
+    elif dy > DY_LIM[2]:
+        power = DY_POW[2]
     else:
-        power = DH_POW[3]
+        power = DY_POW[3]
 
     # dx < 0 if landing site on the right of the shuttle
-    # h_speed > 0 when the shuttle is going on the right
+    # Vx > 0 when the shuttle is going on the right
+    Vx_max = 0
+    Vy_max = 0
 
     vmax = get_max_speed(dx)
     print("vmax: " + str(vmax), file=sys.stderr, flush=True)
     
     # SPEED LIMITS
-    if h_speed > vmax:
-            rotation = 20
-    if h_speed < vmax:
-            rotation = -20
+    if Vx > vmax:
+            rotation = 40
+    if Vx < vmax:
+            rotation = -40
 
-    if dh < 100:
+    if dy < 100:
         rotation = 0
 
 
     # ALT
-    DH_LIM = [2000, 1500, 1000, 500]
-    ROT_ = [3, 4, 4, 4]
+    # DY_LIM = [2000, 1500, 1000, 500]
+    # ROT_ = [3, 4, 4, 4]
 
-    if abs(v_speed) > 25:
+    if abs(Vy) > 45:
+        power = 4
+        if abs(rotation) > 10:
+            if rotation > 0:
+                rotation = 10
+            else:
+                rotation = -10
+    elif abs(Vy) > 25:
         power = 4
         if abs(rotation) > 20:
             if rotation > 0:
@@ -125,4 +193,30 @@ while True:
             else:
                 rotation = -20
 
-    print(str(rotation) + " " + str(power))
+    print("dh", str(dh), file=sys.stderr, flush=True)
+    # print("Vy/dh", str(Vy / dh), file=sys.stderr, flush=True)
+
+    speed = math.sqrt(Vx*Vx + Vy*Vy)
+
+    print("speed", str(speed), file=sys.stderr, flush=True)
+    if dh and abs(speed) > dh / 10:
+        print("GROUND_PROTECTION", file=sys.stderr, flush=True)
+
+
+    # SPEED PROTECTION
+    if speed > 50:
+        rotation = 90 * math.sin(Vx/speed)
+        power = 4
+
+    if rotation > 60:
+        rotation = 60
+    elif rotation < -60:
+        rotation = -60
+
+    # GROUND PROTECTION
+    gp = ground_proctection(x, y, Vx, Vy, land_x, land_y)
+    if gp:
+        rotation = gp
+        power = 4
+
+    print(str(int(rotation)) + " " + str(power))
